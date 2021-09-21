35893b240a96305be2fd33926793a3bac918ec1d
Christopher Pappas <damassi.pappas@gmail.com>
2021-09-20 22:04:22 -0700

    fix item url

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -59,7 +59,7 @@ export const Shop = () => {
                       data-item-image="/assets/images/starry-night.jpg"
                       data-item-name={product.title}
                       data-item-price={product.price}
-                      data-item-url="https://soulcedarfarm.com/shop"
+                      data-item-url="/"
                     >
                       Add to cart
                     </button>
