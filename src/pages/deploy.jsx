d12f0c0e04a0b1803b9e0580c8626d7304337c64
Christopher Pappas <damassi.pappas@gmail.com>
2021-09-23 22:45:33 -0700

    dont index deploy page

--- src/pages/deploy.jsx
+++ src/pages/deploy.jsx
@@ -1,15 +1,21 @@
 import { Center } from "../components/Center";
+import Head from "next/head";
 
 export default function Deploy() {
   return (
-    <Center>
-      <form
-        target="_blank"
-        action="https://api.netlify.com/build_hooks/614ad256da0f94653daaeef5"
-        method="post"
-      >
-        <input type="submit" name="deploy" value="Deploy Site" />
-      </form>
-    </Center>
+    <>
+      <Head>
+        <meta name="robots" content="noindex"></meta>
+      </Head>
+      <Center>
+        <form
+          target="_blank"
+          action="https://api.netlify.com/build_hooks/614ad256da0f94653daaeef5"
+          method="post"
+        >
+          <input type="submit" name="deploy" value="Deploy Site" />
+        </form>
+      </Center>
+    </>
   );
 }
